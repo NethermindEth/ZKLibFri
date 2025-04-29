dfddeea2da94411b8c7dc069931a0f0cc32b6a27
Katy Hristova <katerina.hristova@nethermind.io>
2025-04-29 13:27:12 +0100

    reorg of ReedSolomonCodes

--- ArkLib/Data/CodingTheory/ReedSolomonCodes.lean
+++ ArkLib/Data/CodingTheory/ReedSolomonCodes.lean
@@ -1,56 +1,56 @@
-import CodingTheory.RelativeHammingDistance
-import CodingTheory.LinearCodes
+import ArkLib.Data.CodingTheory.LinearCodes
 import ArkLib.Data.CodingTheory.ReedSolomon
-
-namespace RSCodes
+import ArkLib.Data.CodingTheory.RelativeHammingDistance
 
 open Classical
 
-variable {ι : Type*} [Fintype ι]
-         {F : Type*}
-         {C : Set (ι → F)}
+variable {ι : ℕ}
+         {F : Type*} [Semiring F]
+         {C : Set (Fin ι → F)}
 
-abbrev LinearCode.{u, v} (ι : Type u) (F : Type v) [Semiring F] : Type (max u v) := Submodule F (ι → F)
+abbrev LinearCode.{u} (F : Type u) [Semiring F] : Type u := Submodule F ((Fin ι) → F)
 
 noncomputable section
 
+namespace Vandermonde
 /--
-  n x deg Vandermonde matrix
+  `ι x deg` Vandermonde matrix
 -/
-def vandermonde_nonsq [Semiring F] {deg : ℕ} {n : ℕ} (α : Fin n ↪ F) : Matrix (Fin n) (Fin deg) F :=
+def nonsquare {deg : ℕ} (α : Fin ι ↪ F) : Matrix (Fin ι) (Fin deg) F :=
   Matrix.of (fun i j => (α i) ^ j.1)
 
 /--
-  The transpose of an n x deg Vandermonde matrix
+  The transpose of a `ι x deg` Vandermonde matrix
 -/
-def transpose_vandermonde_nonsq [Field F] {deg : ℕ} {n : ℕ} (α : Fin n ↪ F) :
-  Matrix (Fin deg) (Fin n) F :=
-  (vandermonde_nonsq α).transpose
+def nonsquareTranspose [Field F] {deg : ℕ} (α : Fin ι ↪ F) :
+  Matrix (Fin deg) (Fin ι) F :=
+  (Vandermonde.nonsquare α).transpose
+
+end Vandermonde
+
+namespace ReedSolomonCode
 
 /--
-  The transposed Vandermonde matrix is the generator matrix for an RS code of length n and dimension deg.
-  Hence, RS codes are linear codes.
+The transposed of the  Vandermonde matrix is the generator matrix for an RS code of length n and dimension deg.
 -/
-lemma vandermondeIsGenOfRS' [Field F] {deg : ℕ} {n : ℕ} (α : Fin n ↪ F) :
-  C(transpose_vandermonde_nonsq (deg := deg) α) = ReedSolomon.code α deg := sorry
+lemma genMatIsVandermondeT [Field F] {deg : ℕ} (α : Fin ι ↪ F) :
+  C(Vandermonde.nonsquareTranspose (deg := deg) α) = ReedSolomon.code α deg := by sorry
 
 /--
   The minimal code distance of an RS code of length n and dim deg is n - deg + 1
 -/
-lemma minCodeDistRS [Field F] (deg : ℕ) {n : ℕ} (α : Fin n ↪ F) :
-  RelativeHamming.codeDistLin (ReedSolomon.code α deg) = n - deg + 1 := sorry
+lemma minDist [Field F] (deg : ℕ) (α : Fin ι ↪ F) :
+  RelativeHamming.codeDistLin (ReedSolomon.code α deg) = ι - deg + 1 := sorry
 
-/--
- The dimension of an RS code is deg
--/
-lemma dimRSCode [Field F] {deg : ℕ} {n : ℕ} {α : Fin n ↪ F} :
+
+lemma dim [Field F] {deg : ℕ} {α : Fin ι ↪ F} :
   LinearCodes.dimLinCode (ReedSolomon.code α deg) = deg := sorry
 
 /--
 rate of RS code
 -/
-def rateRS [Field F] (deg : ℕ) {n : ℕ} (α : Fin n ↪ F) : ℚ :=
+def rate [Field F] (deg : ℕ) (α : Fin ι ↪ F) : ℚ :=
   LinearCodes.rateCode (ReedSolomon.code α deg)
 
+end ReedSolomonCode
 end
-end RSCodes
