7507cffe18c7e22492a69f0f7a76833089d38fc9
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-05-01 18:33:26 +0100

    WIP

--- ArkLib/Data/CodingTheory/BerlekampWelch.lean
+++ ArkLib/Data/CodingTheory/BerlekampWelch.lean
@@ -4,6 +4,8 @@ import Mathlib.Algebra.Polynomial.Degree.Definitions
 import Mathlib.Algebra.Polynomial.FieldDivision
 import Mathlib.Data.Finset.Insert
 
+import ArkLib.Data.CodingTheory.Basic
+
 variable {F : Type} [Field F] [DecidableEq F]
 
 open Polynomial
@@ -15,14 +17,14 @@ noncomputable def ElocPoly {n : ℕ} {ωs : Fin n → F}
   | n + 1 => List.prod <| List.map (fun i => 
       let i := Fin.ofNat' n.succ i
       let ω := ωs i 
-      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else X) (List.range n.succ) 
+      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else Polynomial.C 1) (List.range n.succ) 
 
 lemma eloc_poly_nonzero_def {n : ℕ} {ωs : Fin n → F} 
   {f : Fin n → F} {p : Polynomial F} (hne: n ≠ 0) :
     ElocPoly (ωs := ωs) f p = (List.prod <| List.map (fun i => 
       let i := @Fin.ofNat' n (neZero_iff.2 hne) i
       let ω := ωs i 
-      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else X) (List.range n) ) := by
+      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else Polynomial.C 1) (List.range n) ) := by
   rcases n with n | n <;> try simp at hne 
   simp [ElocPoly]
 
@@ -34,15 +36,15 @@ lemma eloc_poly_zero {f : Fin 0 → F} {ωs : Fin 0 → F} {p : Polynomial F} :
 lemma eloc_poly_one {f : Fin 1 → F} {ωs : Fin 1 → F} {p : Polynomial F} :
     ElocPoly (ωs := ωs) f p = let i := Fin.ofNat' 1 0
       let ω := ωs i 
-      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else X := by
+      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else Polynomial.C 1 := by
   simp [ElocPoly, List.range_succ]
 
 lemma eloc_poly_two {f : Fin 2 → F} {ωs : Fin 2 → F} {p : Polynomial F} :
     ElocPoly (ωs := ωs) f p = 
       if f 1 = eval (ωs 1) p 
-      then if f 0 = eval (ωs 0) p then X * X 
-           else (X - C (ωs 0)) * X
-      else if f 0 = eval (ωs 0) p then X * (X - C (ωs 1)) 
+      then if f 0 = eval (ωs 0) p then Polynomial.C 1 
+           else (X - C (ωs 0)) 
+      else if f 0 = eval (ωs 0) p then (X - C (ωs 1)) 
            else (X - C (ωs 0)) * (X - C (ωs 1)) := by
   simp [ElocPoly, List.range_succ]
 
@@ -51,7 +53,7 @@ lemma eloc_poly_succ {n : ℕ}
   ElocPoly (ωs := ωs) f p = 
       (ElocPoly (ωs := ωs ∘ Fin.castSucc) (f ∘ Fin.castSucc) p)* (let i := Fin.last n
       let ω := ωs i 
-      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else X) := by
+      if f i ≠ p.eval ω then (X - (Polynomial.C ω)) else Polynomial.C 1) := by
   conv => 
     lhs 
     unfold ElocPoly; rfl
@@ -110,20 +112,19 @@ lemma roots_of_eloc_poly
   {ωs : Fin n → F} 
   {p : Polynomial F} 
   {x : F} 
-  (hne : x ≠ 0)  
-    (h : (ElocPoly (ωs := ωs) f p).eval x = 0) : 
+  (h : (ElocPoly (ωs := ωs) f p).eval x = 0) : 
   ∃ i : Fin n, f i ≠ p.eval (ωs i)  := by
   revert f p x ωs 
   induction n with
   | zero =>
     aesop
   | succ n ih =>
-    intro ωs f p x hne heval
+    intro ωs f p x heval
     rw [eloc_poly_succ
     , Polynomial.eval_mul
     , mul_eq_zero] at heval
     rcases heval with heval | heval
-    · specialize (ih hne heval)
+    · specialize (ih heval)
       rcases ih with ⟨i, ih⟩
       exists i
       simp at ih
@@ -132,7 +133,6 @@ lemma roots_of_eloc_poly
       by_cases hif : ωs (Fin.last n) = eval (f (Fin.last n)) p
         <;> simp [hif] at heval
       · tauto
-      · exists (Fin.last n)
 
 lemma errors_are_roots_of_eloc_poly 
   {n : ℕ} 
@@ -140,14 +140,13 @@ lemma errors_are_roots_of_eloc_poly
   {ωs : Fin n → F} 
   {p : Polynomial F} 
   {i : Fin n} 
-  (hne : ωs i ≠ 0)
   (h : f i ≠ p.eval (ωs i)) :
   (ElocPoly (ωs := ωs) f p).eval (ωs i) = 0 := by
   revert f ωs i p
   induction n with
   | zero => simp [ElocPoly]
   | succ n ih => 
-    intro f ωs p i hne h
+    intro f ωs p i h
     rw [eloc_poly_succ]
     simp [Polynomial.eval_mul]
     by_cases hi : i = Fin.last n
@@ -165,10 +164,10 @@ lemma errors_are_roots_of_eloc_poly
       rw [hj]
       by_cases hif : f (Fin.last n) = eval (ωs (Fin.last n)) p 
         <;> try simp only [hif, ite_true, ite_false, Polynomial.eval_mul, mul_eq_zero]
+      · apply ih 
+         ; try simp [Function.comp, h]
       · left ; apply ih 
-         <;> try simp [Function.comp, hne, h]
-      · left ; apply ih 
-         <;> try simp [Function.comp, hne, h]
+         ; try simp [Function.comp, h]
 
 variable 
   {n : ℕ} 
@@ -200,14 +199,15 @@ lemma eloc_poly_ne_zero :
 
 @[simp]
 lemma eloc_poly_deg 
-  {n : ℕ} 
-  {f : Fin n → F} 
-  {ωs : Fin n → F} 
-  {p : Polynomial F} 
-  : (ElocPoly (ωs := ωs) f p).natDegree = n := by
+  : (ElocPoly (ωs := ωs) f p).natDegree = Δ₀(f, p.eval ∘ ωs) := by
   revert f ωs p 
+  unfold Function.comp
   induction n with
-  | zero => simp [ElocPoly]
+  | zero => 
+    simp only [eloc_poly_zero, map_one, natDegree_one, hamming_zero_eq_dist] 
+    intro f ωs p
+    ext x
+    exact Fin.elim0 x
   | succ n ih => 
     intro f ωs p 
     rw [eloc_poly_succ
@@ -225,4 +225,42 @@ lemma eloc_poly_deg
       exact Polynomial.X_ne_C _ h
     })]
     by_cases hif: f (Fin.last n) = eval (ωs (Fin.last n)) p
-      <;> try simp [hif, ih]
+      <;> try simp only [hif, ih]
+    · simp [hammingDist]
+      apply Finset.card_eq_of_equiv
+      simp
+      exact ⟨fun ⟨x, hx⟩ => ⟨x, by simp [hx]⟩
+        , fun ⟨x, hx⟩ => ⟨⟨x.val,by {
+            have hx_le : x.val ≤ n := Fin.le_last _
+            have hx_ne : x.val ≠ n := by {
+              intro contr
+              have hx_eq : x = Fin.last n := by {
+                apply Fin.ext
+                simp [contr]
+              }
+              subst hx_eq
+              tauto
+            }
+            omega
+          }⟩,  by aesop⟩, by simp [Function.LeftInverse], by simp [Function.RightInverse, Function.LeftInverse] ⟩
+    · simp [hif, hammingDist]
+      symm
+      rw [Finset.card_eq_succ]
+      exists (Fin.last n)
+      exists Finset.filter (fun i => ¬f i = eval (ωs i) p ∧ i ≠ Fin.last n) (Fintype.elems (α := Fin n.succ)) 
+      apply And.intro <;> try simp
+      apply And.intro
+      · apply Finset.ext
+        intro a
+        apply Iff.intro <;> intro hhh
+        · rw [Finset.mem_insert] at hhh 
+          rcases hhh with hhh | hhh
+          subst hhh
+          simp [hif]
+          simp at hhh
+          simp [hhh]
+        · simp at hhh
+          simp
+          by_cases haa : a = Fin.last n <;> try simp [haa, Fintype.elems, hhh]
+      · 
+
