ef0b06441d30303c3d82e9a6938aeaf38d747293
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-05-06 11:09:22 +0200

    only export ElocPolyF and elocPoly_deg; the rest needs to be explicitly brought in

--- ArkLib/Data/CodingTheory/BerlekampWelch.lean
+++ ArkLib/Data/CodingTheory/BerlekampWelch.lean
@@ -31,26 +31,28 @@ variable [Zero α] {f : ℕ → α} {f' : Fin n → α}
   `liftF` lifts functions over domains `Fin n` to functions over domains `ℕ`
   by returning `0` on points `≥ n`.
 -/
-def liftF (f : Fin n → α) : ℕ → α :=
+protected def liftF (f : Fin n → α) : ℕ → α :=
   fun m ↦ if h : m < n then f ⟨m, h⟩ else 0
 
 /--
   `liftF'` lifts functions over domains `ℕ` to functions over domains `Fin n`
   by taking the obvious injection.
 -/
-def liftF' (f : ℕ → α) : Fin n → α :=
+protected def liftF' (f : ℕ → α) : Fin n → α :=
   fun m ↦ f m.1
 
+open BerlekampWelch (liftF liftF')
+
 @[simp]
-lemma liftF_succ {f : Fin (n + 1) → α} : liftF f n = f ⟨n, Nat.lt_add_one _⟩ := by
+protected lemma liftF_succ {f : Fin (n + 1) → α} : liftF f n = f ⟨n, Nat.lt_add_one _⟩ := by
   aesop (add simp liftF)
 
-lemma liftF'_liftF_of_lt {k : Fin m} (h : k < n) :
+protected lemma liftF'_liftF_of_lt {k : Fin m} (h : k < n) :
   liftF' (n := m) (liftF (n := n) f') k = f' ⟨k, by omega⟩ := by
   aesop (add simp [liftF, liftF'])
 
 @[simp]
-lemma liftF'_liftF_succ {f : Fin (n + 1) → α} {x : Fin n} :
+protected lemma liftF'_liftF_succ {f : Fin (n + 1) → α} {x : Fin n} :
   liftF' (liftF (n := n + 1) f) x = f x.castSucc := by
   aesop (add simp [liftF, liftF']) (add safe (by omega))
 
@@ -58,22 +60,25 @@ lemma liftF'_liftF_succ {f : Fin (n + 1) → α} {x : Fin n} :
 protected lemma liftF'_liftF : Function.LeftInverse liftF' (liftF (α := α) (n := n)) := by
   aesop (add simp [Function.LeftInverse, liftF, liftF'])
 
-lemma liftF_liftF'_of_lt (h : m < n) : liftF (liftF' (n := n) f) m = f m := by
+protected lemma liftF_liftF'_of_lt (h : m < n) : liftF (liftF' (n := n) f) m = f m := by
   aesop (add simp liftF)
 
 @[simp]
-lemma liftF_liftF'_succ : liftF (liftF' (n := n + 1) f) n = f n := by
+protected lemma liftF_liftF'_succ : liftF (liftF' (n := n + 1) f) n = f n := by
   aesop (add simp liftF)
 
-abbrev contract (m : ℕ) (f : Fin n → α) := liftF (liftF' (n := m) (liftF f))
+protected abbrev contract (m : ℕ) (f : Fin n → α) := liftF (liftF' (n := m) (liftF f))
 
-lemma contract_eq_liftF_of_lt {k : ℕ} {f : Fin n → α} (h₁ : k < m) :
-  contract m f k = liftF f k := by
+open BerlekampWelch (contract)
+
+protected lemma contract_eq_liftF_of_lt {k : ℕ} (h₁ : k < m) :
+  contract m f' k = liftF f' k := by
   aesop (add simp [contract, liftF, liftF'])
 
 attribute [simp] contract.eq_def
 
-lemma eval_liftF_of_lt {ωs : Fin m → F} (h : n < m) : eval (liftF ωs n) p = eval (ωs ⟨n, h⟩) p := by
+protected lemma eval_liftF_of_lt {f : Fin m → F} (h : n < m) :
+  eval (liftF f n) p = eval (f ⟨n, h⟩) p := by
   aesop (add simp liftF)
 
 end Hoist
@@ -82,7 +87,7 @@ section BW
 
 variable [DecidableEq F]
 
-noncomputable def ElocPoly (n : ℕ) (ωs f : ℕ → F) (p : Polynomial F) : Polynomial F :=
+protected noncomputable def ElocPoly (n : ℕ) (ωs f : ℕ → F) (p : Polynomial F) : Polynomial F :=
   List.prod <| (List.range n).map fun i => 
     if f i = p.eval (ωs i)
     then 1
@@ -90,18 +95,20 @@ noncomputable def ElocPoly (n : ℕ) (ωs f : ℕ → F) (p : Polynomial F) : Po
 
 section
 
+open BerlekampWelch (ElocPoly)
+
 variable {ωs f : ℕ → F}
 
 @[simp]
-lemma elocPoly_zero : ElocPoly 0 ωs f p = 1 := rfl
+protected lemma elocPoly_zero : ElocPoly 0 ωs f p = 1 := rfl
 
 @[simp]
-lemma elocPoly_one :
+protected lemma elocPoly_one :
   ElocPoly 1 ωs f p = if f 0 ≠ p.eval (ωs 0) then X - (C (ωs 0)) else 1 := by
   simp [ElocPoly, List.range_succ]
 
 @[simp]
-lemma elocPoly_two :
+protected lemma elocPoly_two :
   ElocPoly 2 ωs f p = 
   if f 1 = eval (ωs 1) p 
   then if f 0 = eval (ωs 0) p then 1 
@@ -111,7 +118,7 @@ lemma elocPoly_two :
   simp [ElocPoly, List.range_succ]
 
 @[simp]
-lemma elocPoly_succ :
+protected lemma elocPoly_succ :
   ElocPoly (n + 1) ωs f p =
   ElocPoly n ωs f p * 
     if f n = p.eval (ωs n)
@@ -121,7 +128,8 @@ lemma elocPoly_succ :
   rw [List.range_succ, List.map_append, List.prod_append, ←ElocPoly.eq_def]
   simp
 
-lemma roots_of_eloc_poly {x : F}
+open BerlekampWelch (elocPoly_succ) in
+protected lemma roots_of_eloc_poly {x : F}
   (h : (ElocPoly n ωs f p).eval x = 0) : 
   ∃ i, i < n ∧ f i ≠ p.eval (ωs i) := by
   induction' n with n ih generalizing x
@@ -132,7 +140,7 @@ lemma roots_of_eloc_poly {x : F}
       aesop (add safe [(by existsi i), (by omega)])
     · aesop (add safe (by use n))
 
-lemma errors_are_roots_of_elocPoly {i : ℕ}
+protected lemma errors_are_roots_of_elocPoly {i : ℕ}
   (hi : i < n) (h : f i ≠ p.eval (ωs i)) : (ElocPoly n ωs f p).eval (ωs i) = 0 := by
   induction' n with n ih
   · aesop
@@ -142,14 +150,16 @@ lemma errors_are_roots_of_elocPoly {i : ℕ}
       aesop
 
 @[simp]
-lemma elocPoly_ne_zero : ElocPoly n ωs f p ≠ 0 := by
+protected lemma elocPoly_ne_zero : ElocPoly n ωs f p ≠ 0 := by
   induction' n with n _
   · simp
   · aesop (add simp [sub_eq_zero]) (add safe forward (Polynomial.X_ne_C (ωs n)))
 
 section
 
-lemma elocPoly_congr {ωs' f' : ℕ → F}
+open BerlekampWelch (liftF liftF' contract liftF_liftF'_of_lt liftF_liftF'_of_lt)
+
+protected lemma elocPoly_congr {ωs' f' : ℕ → F}
   (h₁ : ∀ {m}, m < n → ωs m = ωs' m) (h₂ : ∀ {m}, m < n → f m = f' m) :
   ElocPoly n ωs f = ElocPoly n ωs' f' := by
   ext p
@@ -161,26 +171,33 @@ lemma elocPoly_congr {ωs' f' : ℕ → F}
   ]
   aesop (add simp List.mem_range)
 
+open BerlekampWelch (elocPoly_congr contract_eq_liftF_of_lt)
+
 noncomputable def ElocPolyF (ωs f : Fin n → F) (p : Polynomial F) : Polynomial F :=
   ElocPoly n (liftF ωs) (liftF f) p
 
 @[simp]
-lemma elocPolyF_eq_elocPoly : ElocPolyF (n := n) (liftF' ωs) (liftF' f) = ElocPoly n ωs f := 
+protected lemma elocPolyF_eq_elocPoly :
+  ElocPolyF (n := n) (liftF' ωs) (liftF' f) = ElocPoly n ωs f := 
   elocPoly_congr liftF_liftF'_of_lt liftF_liftF'_of_lt
 
 @[simp]
-lemma elocPolyF_eq_elocPoly' {ωs f : Fin n → F} :
+protected lemma elocPolyF_eq_elocPoly' {ωs f : Fin n → F} :
   ElocPolyF ωs f p = ElocPoly n (liftF ωs) (liftF f) p := rfl
 
-lemma elocPoly_leftF_leftF_eq_contract {ωs f : Fin m → F} :
+protected lemma elocPoly_leftF_leftF_eq_contract {ωs f : Fin m → F} :
   ElocPoly n (liftF ωs) (liftF f) =
   ElocPoly n (contract n ωs) (contract n f) := by
   rw [elocPoly_congr contract_eq_liftF_of_lt contract_eq_liftF_of_lt]
   
 end
 
+open BerlekampWelch
+  (elocPolyF_eq_elocPoly' elocPoly_leftF_leftF_eq_contract
+   elocPoly_zero elocPoly_succ liftF liftF_succ)
+
 @[simp]
-lemma eloc_poly_deg {ωs f : Fin n → F} : (ElocPolyF ωs f p).natDegree = Δ₀(f, p.eval ∘ ωs) := by
+lemma elocPolyF_deg {ωs f : Fin n → F} : (ElocPolyF ωs f p).natDegree = Δ₀(f, p.eval ∘ ωs) := by
   rw [elocPolyF_eq_elocPoly']
   induction' n with n ih
   · simp only [elocPoly_zero, map_one, natDegree_one, hamming_zero_eq_dist]
