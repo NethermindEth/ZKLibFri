bcfebeae97aa9aa90b852cbf1a2be3529d5ff546
Katy Hristova <katerina.hristova@nethermind.io>
2025-05-07 15:11:18 +0100

    WIP

--- ArkLib/Data/CodingTheory/Prelims.lean
+++ ArkLib/Data/CodingTheory/Prelims.lean
@@ -3,6 +3,8 @@ import Mathlib.Data.Matrix.Defs
 import Mathlib.Data.Matrix.RowCol
 import Mathlib.Order.CompletePartialOrder
 import Mathlib.Algebra.Lie.OfAssociative
+import Mathlib.LinearAlgebra.Dimension.Finrank
+import Mathlib.Data.Matrix.Rank
 
 open Classical
 
@@ -23,10 +25,24 @@ end Affine
 
 namespace Matrices
 
+def neqCols (U V : Matrix (Fin κ) (Fin ι) F) : Finset (Fin ι) :=
+  {j | ∃ i : (Fin κ), V i j ≠ U i j}
+
 def rowSpan (U : Matrix (Fin κ) (Fin ι) F) : Submodule F (Fin ι → F) :=
   Submodule.span F {U i | i : Fin κ}
 
-def neqCols (U V : Matrix (Fin κ) (Fin ι) F) : Finset (Fin ι) :=
-  {j | ∃ i : (Fin κ), V i j ≠ U i j}
+def rowRank (U : Matrix (Fin κ) (Fin ι) F) : ℕ :=
+  Module.finrank F (rowSpan U)
+
+def colSpan (U : Matrix (Fin κ) (Fin ι) F) : Submodule F (Fin κ → F) :=
+  Submodule.span F {Matrix.transpose U i | i : Fin ι}
+
+def colRank (U : Matrix (Fin κ) (Fin ι) F) : ℕ :=
+  Module.finrank F (colSpan U)
+
+lemma rank_eq_min_row_col_rank [CommRing F] (U : Matrix (Fin κ) (Fin ι) F) :
+  U.rank = min (rowRank U) (colRank U) := by sorry
+
+
 
 end Matrices
